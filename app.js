ea4f9e786fab958f5138cc34952ea0c0ae2567f2
Kelvin Chua <chuakid@gmail.com>
2020-06-08 13:39:02 +0800

    code cleanup

--- app.js
+++ app.js
@@ -10,45 +10,44 @@ if (port == null || port == "") {
     port = 3000;
 }
 
-let rooms = [1,2,3,4], roomlinks = {}
+let rooms = [1, 2, 3, 4], roomInfo = {
+
+}
+
+for (let room of rooms) {
+    roomInfo[room] = {}
+}
 
 io.on('connection', (client) => {
     client.emit("rooms", rooms)
     client.on("join", (info) => {
-        if(!rooms.includes(parseInt(info.roomId)))
+        if (!rooms.includes(parseInt(info.roomId)))
             return
-        
+
         client.join(info.roomId)
         client.roomId = info.roomId
         console.log(client.id + " joined " + client.roomId)
         client.emit("roomJoinStatus")
-        if (!roomlinks[client.roomId]) {
-            roomlinks[client.roomId] = {}
-        }
-        else if (roomlinks[client.roomId]['link']){
-            client.emit("linkChanged", roomlinks[client.roomId]["link"])
-        }
+
+        if (roomInfo[client.roomId]['link'])
+            client.emit("linkChanged", roomInfo[client.roomId]["link"])
         client.broadcast.to(client.roomId).emit("clientJoined", info.name)
     })
     client.on("linkChange", (id) => {
-        roomlinks[client.roomId]["link"] = id
+        roomInfo[client.roomId]["link"] = id
         client.broadcast.to(client.roomId).emit("linkChanged", id)
     })
     client.on("seek", (seekTime) => {
-        console.log("Seek Time changed to " + seekTime + " by " + client.id)
         client.broadcast.to(client.roomId).emit("seek", seekTime)
     })
     client.on("playing", () => {
-        console.log("Client " + client.id + " playing")
         client.broadcast.to(client.roomId).emit("playing")
     })
     client.on("pause", () => {
-        console.log("Client " + client.id + " paused")
         client.broadcast.to(client.roomId).emit("pause")
     })
     client.on("disconnect", () => {
-        console.log("a user disconnected")
-
+        console.log(client + " disconnected")
     })
     console.log("a user connected")
 });
